eec30d601cdf17c22855559729492cd6d0e3ba68
Dell <Dell@DESKTOP-9VEI7R1>
2022-11-21 13:54:40 +0530

    Commit-Demo5

---
+++ src/test/java/Demo/DemoTest.java
@@ -0,0 +1,19 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+    @Test
+		public void t (){
+		     
+			
+				System.out.println("Hi This is framework 6");
+				System.out.println("");
+
+			}
+
+
+
+	}
+
